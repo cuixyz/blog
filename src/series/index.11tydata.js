6df077e4760089a28477e07b72fd4d38b95bd392
翠翔羽翥 <mail@cuixyz.com>
2026-09-10 09:31:40 +0800

    Delete src/series/index.11tydata.js

--- src/series/index.11tydata.js
+++
@@ -1,8 +0,0 @@
-import { getVisibleSeriesList } from '../../lib/series.js';
-
-export default {
-  eleventyComputed: {
-    visibleSeries: (data) =>
-      getVisibleSeriesList(data?.series, data?.environment || 'development'),
-  },
-};
